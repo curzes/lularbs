043b9df84207c76180197d31aa292a4ebc3deee1
curzes <markuspersson22@gmail.com>
2021-02-16 13:34:51 +0100

    #

--- larbs.sh
+++ larbs.sh
@@ -15,6 +15,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 esac done
 
 [ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/curzes/.dot-test.git"
+[ -z "$dotfilesrepossh" ] && dotfilesrepossh="git@github.com:curzes/.dot-test.git"
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/curzes/lularbs/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="yay"
 [ -z "$repobranch" ] && repobranch="master"
@@ -29,7 +30,7 @@ welcomemsg() { \
 	dialog --title "Welcome!" --msgbox "Welcome to Luke's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Luke" 10 60
 
 	dialog --colors --title "Important Note!" --yes-label "All ready!" --no-label "Return..." --yesno "Be sure the computer you are using has current pacman updates and refreshed Arch keyrings.\\n\\nIf it does not, the installation of some programs might fail." 8 70
-	}
+}
 
 getuserandpass() { \
 	# Prompts user for new username an password.
@@ -43,16 +44,18 @@ getuserandpass() { \
 		unset pass2
 		pass1=$(dialog --no-cancel --passwordbox "Passwords do not match.\\n\\nEnter password again." 10 60 3>&1 1>&2 2>&3 3>&1)
 		pass2=$(dialog --no-cancel --passwordbox "Retype password." 10 60 3>&1 1>&2 2>&3 3>&1)
-	done ;}
+	done ;
+}
 
 usercheck() { \
 	! { id -u "$name" >/dev/null 2>&1; } ||
 	dialog --colors --title "WARNING!" --yes-label "CONTINUE" --no-label "No wait..." --yesno "The user \`$name\` already exists on this system. LARBS can install for a user already existing, but it will \\Zboverwrite\\Zn any conflicting settings/dotfiles on the user account.\\n\\nLARBS will \\Zbnot\\Zn overwrite your user files, documents, videos, etc., so don't worry about that, but only click <CONTINUE> if you don't mind your settings being overwritten.\\n\\nNote also that LARBS will change $name's password to the one you just gave." 14 70
-	}
+}
 
 preinstallmsg() { \
-	dialog --title "Let's get this party started!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || { clear; exit 1; }
-	}
+	dialog --title "Let's get this party started!" --yes-label "Let's go!" --no-label "No, nevermind!" --yesno "The rest of the installation will now be totally automated, so you can sit back and relax.\\n\\nIt will take some time, but when done, you can relax even more with your complete system.\\n\\nNow just press <Let's go!> and the system will begin installation!" 13 60 || { clear; exit 1;
+}
+}
 
 adduserandpass() { \
 	# Adds user `$name` with password $pass1.
@@ -61,17 +64,19 @@ adduserandpass() { \
 	usermod -a -G wheel,uucp,lock "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
 	repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel "$(dirname "$repodir")"
 	echo "$name:$pass1" | chpasswd
-	unset pass1 pass2 ;}
+	unset pass1 pass2 ;
+}
 
 refreshkeys() { \
 	dialog --infobox "Refreshing Arch Keyring..." 4 40
 	pacman -Q artix-keyring >/dev/null 2>&1 && pacman --noconfirm -S artix-keyring >/dev/null 2>&1
 	pacman --noconfirm -S archlinux-keyring >/dev/null 2>&1
-	}
+}
 
 newperms() { # Set special sudoers settings for install (or after).
 	sed -i "/#LARBS/d" /etc/sudoers
-	echo "$* #LARBS" >> /etc/sudoers ;}
+	echo "$* #LARBS" >> /etc/sudoers ;
+}
 
 bootloadersetup(){
 	pacman --noconfirm -S gptfdisk syslinux
@@ -88,8 +93,8 @@ setlanguage(){
 	sed -i 's/#sv_SE.UTF-8 UTF-8/sv_SE.UTF-8 UTF-8/g' /etc/locale.gen
 	sed -i 's/#sv_SE ISO-8859-1/sv_SE ISO-8859-1/g' /etc/locale.gen
 	locale-gen
-	echo KEYMAP=sv-latin1 > /etc/locale.conf
-	echo LANG=sv_SE.UTF-8 > /etc/locale.conf
+	echo KEYMAP=sv-latin1 >> /etc/locale.conf
+	echo LANG=sv_SE.UTF-8 >> /etc/locale.conf
 	export LANG=sv_SE.UTF-8
 	ln -s /usr/share/zoneinfo/Europe/Stockholm /etc/localtime
 }
@@ -103,12 +108,13 @@ manualinstall() { # Installs $1 manually if not installed. Used only for AUR hel
 	sudo -u "$name" tar -xvf "$1".tar.gz >/dev/null 2>&1 &&
 	cd "$1" &&
 	sudo -u "$name" makepkg --noconfirm -si >/dev/null 2>&1
-	cd /tmp || return 1) ;}
+	cd /tmp || return 1) ;
+}
 
 maininstall() { # Installs all needed programs from main repo.
 	dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total). $1 $2" 5 70
 	installpkg "$1"
-	}
+}
 
 gitmakeinstall() {
 	progname="$(basename "$1" .git)"
@@ -118,19 +124,20 @@ gitmakeinstall() {
 	cd "$dir" || exit 1
 	make >/dev/null 2>&1
 	make install >/dev/null 2>&1
-	cd /tmp || return 1 ;}
+	cd /tmp || return 1 ;
+}
 
 aurinstall() { \
 	dialog --title "LARBS Installation" --infobox "Installing \`$1\` ($n of $total) from the AUR. $1 $2" 5 70
 	echo "$aurinstalled" | grep -q "^$1$" && return 1
 	sudo -u "$name" $aurhelper -S --noconfirm "$1" >/dev/null 2>&1
-	}
+}
 
 pipinstall() { \
 	dialog --title "LARBS Installation" --infobox "Installing the Python package \`$1\` ($n of $total). $1 $2" 5 70
 	[ -x "$(command -v "pip")" ] || installpkg python-pip >/dev/null 2>&1
 	yes | pip install "$1"
-	}
+}
 
 installationloop() { \
 	([ -f "$progsfile" ] && cp "$progsfile" /tmp/progs.csv) || curl -Ls "$progsfile" | sed '/^#/d' > /tmp/progs.csv
@@ -145,30 +152,40 @@ installationloop() { \
 			"P") pipinstall "$program" "$comment" ;;
 			*) maininstall "$program" "$comment" ;;
 		esac
-	done < /tmp/progs.csv ;}
+	done < /tmp/progs.csv ;
+}
 
-# putgitrepo() { # Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
+putgitrepo() {
+	# Downloads a gitrepo $1 and places the files in $2 only overwriting conflicts
 
-	# putgitrepo "$dotfilesrepo" "/home/$name" "$repobranch"
+	dialog --infobox "Downloading and installing config files..." 4 60
 
-	# dialog --infobox "Downloading and installing config files..." 4 60
-	# [ -z "$3" ] && branch="master" || branch="$repobranch"
-	# dir=$(mktemp -d)
-	# [ ! -d "$2" ] && mkdir -p "$2"
-	# chown -R "$name":wheel "$dir" "$2"
-	# sudo -u "$name" git clone --recursive -b "$branch" --depth 1 "$1" "$dir" >/dev/null 2>&1
-	# sudo -u "$name" cp -rfT "$dir" "$2"
+	git clone --bare https://github.com/curzes/.dot-test.git /home/"$name"/.cfg
 
-#}
+	mkdir -p /home/"$name"/.config-backup
+
+	/usr/bin/git --git-dir=/home/"$name"/.cfg/ --work-tree=/home/"$name" checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} /home/"$name"/.config-backup/{}
+
+	/usr/bin/git --git-dir=/home/"$name"/.cfg/ --work-tree=/home/"$name" checkout
+	/usr/bin/git --git-dir=/home/"$name"/.cfg/ --work-tree=/home/"$name" config status.showUntrackedFiles no
+
+	/usr/bin/git --git-dir=/home/"$name"/.cfg/ --work-tree=/home/"$name" remote set-url origin git@github.com:curzes/.dot-test.git
+
+	rm -rf /home/"$name"/.config-backup
+
+	mkdir /home/"$name"/Downloads
+	mkdir /home/"$name"/session
+}
 
 systembeepoff() { dialog --infobox "Getting rid of that retarded error beep sound..." 10 50
 	rmmod pcspkr
-	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;}
+	echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf ;
+}
 
 finalize(){ \
 	dialog --infobox "Preparing welcome message..." 4 50
 	dialog --title "All done!" --msgbox "Congrats! Provided there were no hidden errors, the script completed successfully and all the programs and configuration files should be in place.\\n\\nTo run the new graphical environment, log out and log back in as your new user, then run the command \"startx\" to start the graphical environment (it will start automatically in tty1).\\n\\n.t Luke" 12 80
-	}
+}
 
 ### THE ACTUAL SCRIPT ###
 
